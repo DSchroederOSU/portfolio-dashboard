45ce751a87668898ca3d546e3908f5e6796b87af
DSchroederOSU <schrodan@oregonstate.edu>
2019-01-23 15:54:57 -0800

    rm node m

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = 3000
 app.use('/assets', express.static('assets'))
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { 
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3000);s
+app.listen(3000);
