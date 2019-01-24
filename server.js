92a55dae966353fbdfdb674438d8f05cffcb50e7
DSchroederOSU <schrodan@oregonstate.edu>
2019-01-23 18:10:31 -0800

    vue

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const express = require('express')
-let app = express()
-
-const path = require('path')
-
-let PORT = process.env.PORT
-app.use('/assets', express.static('assets'))
-
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-app.listen(PORT);
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
