444de37bd7341c68868cb743a5cb02500d6b29fc
DSchroederOSU <schrodan@oregonstate.edu>
2019-01-23 18:40:50 -0800

    naviagtion

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TopNav/>
+    <SideNav/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import SideNav from '@/components/SideNav.vue'
+import TopNav from '@/components/TopNav.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    SideNav,
+    TopNav
   }
 }
 </script>
