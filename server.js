3c4a905142d0099c5e5d31cd99da8e6e0fab5670
DSchroederOSU <schrodan@oregonstate.edu>
2019-01-23 18:29:13 -0800

    favicon

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
-app.listen(port);
+const PORT = process.env.PORT || 3000;
+
+ 
+app.get('/architecture', function(req, res) {
+    console.log("Architecture")
+});
+
+console.log(PORT)
+app.listen(PORT);
