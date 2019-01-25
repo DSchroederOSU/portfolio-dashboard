051cbf230ac0decef87ad00a2767817160feaeb2
DSchroederOSU <schrodan@oregonstate.edu>
2019-01-25 14:29:43 -0800

    dynamic chart

---
+++ src/components/DynamicChart.vue
@@ -0,0 +1,37 @@
+<script>
+
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: Array,
+    labels: Array
+  },
+  mounted () {
+    this.renderChart({
+      labels: this.labels,
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: this.chartdata
+        }
+      ]
+    }, {
+      legend: {
+        display: false
+      },
+      tooltips: {
+          callbacks: {
+             label: function(tooltipItem) {
+                    return tooltipItem.yLabel;
+             }
+          }
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    })
+  }
+}
+</script>
